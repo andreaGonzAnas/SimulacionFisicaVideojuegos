4b8ad296f5a0206a2e7f69587186d35bba941489
Andrea G.A. <andrgo37@ucm.es>
2025-11-28 09:33:14 +0100

    Generado flotacion

---
+++ skeleton/BuoyancyForceGenerator.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "ForceGenerator.h"
+class BuoyancyForceGenerator : public ForceGenerator
+{
+protected:
+	float _height;
+	float _volume;
+	float _liquid_density;
+	float _gravity = 9.8;
+
+	Particula* _liquid_particle;
+
+public:
+	BuoyancyForceGenerator(float h, float v, float d, Particula* liquidParticle);
+	virtual ~BuoyancyForceGenerator() {}
+
+	void update(Particula* particle, double t) override;
+};
+
